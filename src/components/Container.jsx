a70022d280448f3b7e860c91267c4f2c3d233e41
my Name <you@example.com>
2024-06-06 17:50:38 +0300

    first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+function Container() {
+    return (
+        <div className="container">
+            <Comment />
+        </div>
+    );
+}
+
+export default Container;
